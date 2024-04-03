c1a6a66eea9b5e13a2cd1a1c690bffe9ec463ab2
Sky Surfer <athat1256@gmail.com>
2024-04-03 15:54:57 +0700

    feat: use datatable style, add node modal

--- resources/views/nodes/index.blade.php
+++ resources/views/nodes/index.blade.php
@@ -5,6 +5,7 @@
 @endpush
 
 @push('css')
+    <link rel="stylesheet" href="{{ asset('vendor/datatables/dataTables.tailwindcss.min.css') }}">
 @endpush
 
 @push('style')
@@ -17,43 +18,50 @@
 @endpush
 
 @section('content')
+    <div class="flex justify-between flex-wrap mb-4">
+        <h3 class="text-2xl font-bold text-slate-700">Node</h3>
+        <button data-modal-target="node-modal" data-modal-toggle="node-modal"
+            class="block text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+            type="button">
+            <i class="fa-solid fa-plus h-3 me-2 -ms-1"></i>
+            Tamabah Node
+        </button>
+    </div>
     <div class="w-full bg-white rounded-lg shadow">
         <div class="border-b">
-            <h5 class="text-xl font-bold text-gray-900 py-2 px-4 md:py-3 md:px-6">Node</h5>
+            <h5 class="text-lg font-bold text-gray-900 py-2 px-4 md:py-3 md:px-6">Data Node</h5>
         </div>
-        <div class="relative overflow-x-auto m-3">
-            <table class="datatable w-full text-sm text-left text-gray-50">
-                <thead class="text-xs text-gray-700 uppercase font-bold bg-gray-50 dark:bg-gray-70">
-                    <tr class="text-center">
-                        <th scope="col" class="p-3">
+        <div class="relative overflow-x-auto p-2">
+            <table class="datatable w-full">
+                <thead>
+                    <tr>
+                        <th scope="col" class="w-16 text-center uppercase">
                             No
                         </th>
-                        <th scope="col" class="px-6 py-3">
+                        <th scope="col" class="text-center uppercase">
                             Nama Node
                         </th>
-                        <th scope="col" class="px-6 py-3">
+                        <th scope="col" class="text-center uppercase">
                             Sensor
                         </th>
-                        <th scope="col" class="px-6 py-3">
-                            Action
+                        <th scope="col" class="text-center uppercase">
+                            Aksi
                         </th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach ($nodes as $node)
-                        <tr
-                            class="w-4 p-4 text-center odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                            <th scope="row"
-                                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        <tr class="text-center">
+                            <th scope="row" class="text-center">
                                 {{ $loop->iteration }}
                             </th>
-                            <td class="px-6 py-4 text-gray-900">
+                            <td>
                                 {{ $node->name ?? '-' }}
                             </td>
-                            <td class="px-6 py-4 text-gray-900">
+                            <td>
                                 {{ $node->connected_sensor ?? '-' }}
                             </td>
-                            <td class="px-6 py-4 text-gray-900">
+                            <td>
                             </td>
                         </tr>
                     @endforeach
@@ -62,11 +70,25 @@
             </table>
         </div>
     </div>
+
+    @include('nodes.create')
 @endsection
 
 @push('body-js')
     <script src="{{ asset('vendor/sweetalert/sweetalert.all.js') }}"></script>
+    <script src="{{ asset('vendor/datatables/dataTables.min.js') }}"></script>
+    <script src="{{ asset('vendor/datatables/dataTables.tailwindcss.min.js') }}"></script>
+    <script src="{{ asset('vendor/select2/js/select2.min.js') }}"></script>
 @endpush
 
 @push('body-script')
+    <script>
+        $(document).ready(function() {
+            $('.datatable').DataTable({
+                "pageLength": 25,
+            });
+        });
+
+        
+    </script>
 @endpush
