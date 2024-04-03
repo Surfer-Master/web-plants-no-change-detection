46ee3a338862ad931e9ad8debb87af0ead88b085
Sky Surfer <athat1256@gmail.com>
2024-04-03 15:55:42 +0700

    feat: use datatable style; add page header

--- resources/views/plants/index.blade.php
+++ resources/views/plants/index.blade.php
@@ -8,6 +8,7 @@
 @endpush
 
 @push('style')
+    <link rel="stylesheet" href="{{ asset('vendor/datatables/dataTables.tailwindcss.min.css') }}">
 @endpush
 
 @push('head-js')
@@ -17,49 +18,50 @@
 @endpush
 
 @section('content')
+    <div class="flex justify-between flex-wrap mb-4">
+        <h3 class="text-2xl font-bold text-slate-700">Tanaman</h3>
+    </div>
     <div class="w-full bg-white rounded-lg shadow">
         <div class="border-b">
-            <h5 class="text-xl font-bold text-gray-900 py-2 px-4 md:py-3 md:px-6">Tanaman</h5>
+            <h5 class="text-lg font-bold text-gray-900 py-2 px-4 md:py-3 md:px-6">Data Tanaman</h5>
         </div>
-        <div class="relative overflow-x-auto m-3">
-            <table class="datatable w-full text-sm text-left text-gray-50">
-                <thead class="text-xs text-gray-700 uppercase font-bold bg-gray-50 dark:bg-gray-70">
-                    <tr class="text-center">
-                        <th scope="col" class="px-6 py-3" width="1%">
+        <div class="relative overflow-x-auto p-2">
+            <table class="datatable w-full ">
+                <thead>
+                    <tr>
+                        <th scope="col" class="text-center uppercase">
                             No
                         </th>
-                        <th scope="col" class="px-6 py-3">
+                        <th scope="col" class="text-center uppercase">
                             Nama Tanaman
                         </th>
-                        <th scope="col" class="px-6 py-3">
+                        <th scope="col" class="text-center uppercase">
                             Lokasi
                         </th>
-                        <th scope="col" class="px-6 py-3">
+                        <th scope="col" class="text-center uppercase">
                             Urutan Sensor
                         </th>
-                        <th scope="col" class="px-6 py-3">
-                            Action
+                        <th scope="col" class="text-center uppercase">
+                            Aksi
                         </th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach ($plants as $plant)
-                        <tr
-                            class="text-center odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                            <th scope="row"
-                                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        <tr class="text-center">
+                            <th scope="row">
                                 {{ $loop->iteration }}
                             </th>
-                            <td class="px-6 py-4 text-gray-900">
+                            <td>
                                 {{ $plant->name ?? '-' }}
                             </td>
-                            <td class="px-6 py-4 text-gray-900">
+                            <td>
                                 {{ $plant->location ?? '-' }}
                             </td>
-                            <td class="px-6 py-4 text-gray-900">
+                            <td>
                                 {{ $plant->soil_moisture_order ?? '-' }}
                             </td>
-                            <td class="px-6 py-4 text-gray-900">
+                            <td>
                                 <a href="#"
                                     class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
                             </td>
@@ -73,7 +75,17 @@
 
 @push('body-js')
     <script src="{{ asset('vendor/sweetalert/sweetalert.all.js') }}"></script>
+    <script src="{{ asset('vendor/datatables/dataTables.min.js') }}"></script>
+    <script src="{{ asset('vendor/datatables/dataTables.tailwindcss.min.js') }}"></script>
+    <script src="{{ asset('vendor/select2/js/select2.min.js') }}"></script>
 @endpush
 
 @push('body-script')
+    <script>
+        $(document).ready(function() {
+            $('.datatable').DataTable({
+                "pageLength": 25,
+            });
+        });
+    </script>
 @endpush
