2e073febb16fb3de792bc744756ace32671fd584
Sky Surfer <athat1256@gmail.com>
2024-04-16 20:35:00 +0700

    build: add itsgoingd/clockwork

--- config/app.php
+++ config/app.php
@@ -185,6 +185,7 @@ return [
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
         'Alert' => RealRashid\SweetAlert\Facades\Alert::class,
+        'Clockwork' => Clockwork\Support\Laravel\Facade::class,
     ])->toArray(),
 
 ];
