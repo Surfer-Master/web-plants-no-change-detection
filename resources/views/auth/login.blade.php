c6a73a7046cabb4e1a5e1eaf4a65f49fa14850b2
Sky Surfer <athat1256@gmail.com>
2024-04-20 17:40:51 +0700

    style: tambah gambar animasi login (lottie)

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,95 +5,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
-    {{-- <!-- SEO Meta Tags --> --}}
-    {{-- <meta name="description"
-        content="Pavo is a mobile app Tailwind CSS HTML template created to help you present benefits, features and information about mobile apps in order to convince visitors to download them" />
-    <meta name="author" content="Your name" /> --}}
-
-    {{-- <!-- OG Meta Tags to improve the way the post looks when you share the page on Facebook, Twitter, LinkedIn --> --}}
-    {{-- <meta property="og:site_name" content="" /> <!-- website name -->
-    <meta property="og:site" content="" /> <!-- website link -->
-    <meta property="og:title" content="" /> <!-- title shown in the actual shared post -->
-    <meta property="og:description" content="" /> <!-- description shown in the actual shared post -->
-    <meta property="og:image" content="" /> <!-- image link, make sure it's jpg -->
-    <meta property="og:url" content="" /> <!-- where do you want your post to link to -->
-    <meta name="twitter:card" content="summary_large_image" /> <!-- to have large image post format in Twitter --> --}}
-
     <title>{{ $title }}</title>
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-
     <link rel="preconnect" href="https://fonts.gstatic.com" />
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;0,700;1,400&display=swap" />
     <link rel="stylesheet" href="{{ asset('vendor/fontawesome/css/all.min.css') }}" rel="stylesheet">
-
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="{{ asset('css/styles.min.css') }}">
 </head>
 
 <body>
-    {{-- <div class="bg-slate-50 h-screen">
-        <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-            <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-                <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                    alt="Your Company">
-                <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your
-                    account</h2>
-            </div>
-
-            <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                <form class="space-y-6" action="#" method="POST">
-                    <div>
-                        <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email
-                            address</label>
-                        <div class="mt-2">
-                            <input id="email" name="email" type="email" autocomplete="email" required
-                                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                        </div>
-                    </div>
-
-                    <div>
-                        <div class="flex items-center justify-between">
-                            <label for="password"
-                                class="block text-sm font-medium leading-6 text-gray-900">Password</label>
-                            <div class="text-sm">
-                                <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot
-                                    password?</a>
-                            </div>
-                        </div>
-                        <div class="mt-2">
-                            <input id="password" name="password" type="password" autocomplete="current-password"
-                                required
-                                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                        </div>
-                    </div>
-
-                    <div>
-                        <button type="submit"
-                            class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign
-                            in</button>
-                    </div>
-                </form>
-
-                <p class="mt-10 text-center text-sm text-gray-500">
-                    Not a member?
-                    <a href="#" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Start a 14
-                        day
-                        free trial</a>
-                </p>
-            </div>
-        </div>
-    </div> --}}
-
     <section class="bg-gray-50 dark:bg-gray-900 h-screen flex">
         <div class="flex-1 hidden md:block">
-            <div class="flex justify-center">
-
+            <div class="flex h-screen items-center justify-center">
+                <div>
+                    <dotlottie-player src="{{ asset('dotlottie/ZVeyQfBhu0.lottie') }}" background="transparent"
+                        speed="1" style="width: auto; height: auto;" loop autoplay></dotlottie-player>
+                </div>
             </div>
         </div>
         <div class="flex-1 flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen">
-            <a href="/" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+            <a href="/" class="flex items-center mb-6 text-3xl font-semibold text-gray-900 dark:text-white">
                 <img class="w-8 h-8 mr-2 rounded-full" src="{{ asset('img/smart-farming-rounded.png') }}"
                     alt="logo">
                 Smart Farming
@@ -174,6 +107,7 @@
 
     <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
     <script src="{{ asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+    <script src="{{ asset('vendor/dotlottie/player-component/dist/dotlottie-player.mjs') }}" type="module"></script>
     <script src="{{ asset('js/scripts.min.js') }}"></script>
 </body>
 
