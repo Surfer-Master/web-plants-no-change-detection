53d3d79ca13809a531e5a7b86839334d39795db4
Sky Surfer <athat1256@gmail.com>
2024-04-23 20:00:07 +0700

    style: change px-2 pt-2 to p-2 in datatable

--- resources/views/node-send-logs/index.blade.php
+++ resources/views/node-send-logs/index.blade.php
@@ -93,7 +93,7 @@
         <div class="border-b">
             <h5 class="text-lg font-bold text-gray-900 py-2 px-4 md:py-3 md:px-6">Data Log Pengiriman</h5>
         </div>
-        <div class="relative overflow-x-auto px-2 pt-2">
+        <div class="relative overflow-x-auto p-2">
             <table class="datatable w-full">
                 <thead>
                     <tr>
